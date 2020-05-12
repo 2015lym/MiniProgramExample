5b5275d28424b8bef83d145acbe7b09f09100a48
2015lym <devilx2015@gmail.com>
2020-05-12 17:26:34 +0800

    Section5

---
+++ Section5/form/pages/index/index.js
@@ -0,0 +1,59 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+
+  },
+
+  onLoad: function () {
+
+  },
+  testButton() {
+    wx.navigateTo({
+      url: '../test-button/test-button',
+    })
+  },
+  testInput() {
+    wx.navigateTo({
+      url: '../test-input/test-input',
+    })
+  },
+  testTextarea() {
+    wx.navigateTo({
+      url: '../test-textarea/test-textarea',
+    })
+  },
+  testCheckbox() {
+    wx.navigateTo({
+      url: '../test-checkbox/test-checkbox',
+    })
+  },
+  testRadio() {
+    wx.navigateTo({
+      url: '../test-radio/test-radio',
+    })
+  },
+
+  testSlider() {
+    wx.navigateTo({
+      url: '../test-slider/test-slider',
+    })
+  },
+  testSwitch() {
+    wx.navigateTo({
+      url: '../test-switch/test-switch',
+    })
+  },
+  testPicker() {
+    wx.navigateTo({
+      url: '../test-picker/test-picker',
+    })
+  },
+  testEditor() {
+    wx.navigateTo({
+      url: '../test-editor/test-editor',
+    })
+  }
+})

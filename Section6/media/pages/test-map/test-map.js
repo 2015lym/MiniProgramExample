032b4d820e4ed3e103ffb1f7a3a6f2096e9f9e12
2015lym <devilx2015@gmail.com>
2020-06-14 09:39:02 +0800

    更新

--- Section6/media/pages/test-map/test-map.js
+++ Section6/media/pages/test-map/test-map.js
@@ -5,62 +5,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    longitude: '',
+    latitude: '',
+    markers: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var self = this;
+    wx.getLocation({
+      success(res) {
+        console.log(res);
+        var array = [];
+        for (let i = 0; i < 3; i++) {
+          var item = {
+            id: i,
+            title: '我是标记' + i,
+            longitude: res.longitude + i * 0.1,
+            latitude: res.latitude + i * 0.1,
+            width: 40,
+            height: 40,
+          }
+          array.push(item);
+        }
+        self.setData({
+          longitude: res.longitude,
+          latitude: res.latitude,
+          markers: array
+        });
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  markertap(e) {
+    console.log('1111')
+    wx.showToast({
+      title: '点击了标记',
+    })
   }
+
 })

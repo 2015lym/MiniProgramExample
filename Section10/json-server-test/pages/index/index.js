98b06cc11fafd6d9af2a1e4ca908374cd7371c48
2015lym <devilx2015@gmail.com>
2020-07-20 17:00:25 +0800

    Section10

--- Section10/json-server-test/pages/index/index.js
+++ Section10/json-server-test/pages/index/index.js
@@ -4,7 +4,11 @@ Page({
   data: {
     listData: []
   },
-  onLoad: function () {
+  onShow: function () {
+    this.getUsersList()
+  },
+  // 获取用户列表
+  getUsersList() {
     wx.request({
       url: 'http://localhost:3000/users',
       success: (res) => {
@@ -21,6 +25,19 @@ Page({
   // 编辑
   edit(data) {
     console.log(data)
+  },
+  // 删除
+  delete(data) {
+    var userId = data.currentTarget.dataset.item.id;
+    wx.request({
+      method: 'DELETE',
+      url: 'http://localhost:3000/users/' + userId,
+      success: (res) => {
+        wx.showToast({
+          title: '删除成功',
+        })
+      }
+    })
   }
 
 })
